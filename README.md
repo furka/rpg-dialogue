20ae2341bdc3a7b90e82430c9a95c1d858222c8d
Thomas Vranckx <Thomas_Vranckx@discovery.com>
2017-07-20 17:03:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,179 @@
+# rpg-dialogue
+
+### Install
+
+```sh
+$ npm install --save-dev rpg-dialogue
+```
+
+### Description
+An es6 class for handling dialogue trees, useful for RPG games.
+
+### Features
+* Simple interface: `dialogue.interact(id);`
+* Dialogue tree defined in HTML
+* Conditional branches
+* Branch actions
+
+## Usage
+Code:
+
+```js
+import RPGDialogue from 'rpg-dialogue';
+import template from 'demo.pug';
+
+let dialogue = new RPGDialogue(template());
+dialogue.interact(id);                     //interact with a specific dialogue branch
+```
+Dialogue tree:
+
+```pug
+d#0 Hello world
+  d.1 A good day to you too, sir and/or madam!
+  d.2 Bye.
+d#1 Thanks!
+  d.2
+d#2 Bye
+```
+
+
+## API
+
+Instantiate a new dialogue with a template dialogue tree
+
+```js
+let dialogue = new RPGDialogue(template);
+```
+
+### `.interact(id, [conditions, actions])`
+
+Interact with the dialogue tree
+
+```js
+let response = dialogue.interact(0);
+
+```
+
+This will return a `JSON` object containing a response text and subsequent dialogue options
+
+```json
+{
+  "text": "Hello world",
+  "responses": [
+    {
+      "id": 1,
+      "text": "A good day to you too, sir!"
+    },
+    {
+      "id": 2,
+      "text": "Bye."
+    }
+  ]
+}
+```
+
+You would then have the option to continue with `dialogue.interact(1)` or `dialogue.interact(2)`.
+
+## Templates
+Templates are entirely defined as HTML.
+
+To make life easier it is recommended to combine this with your favourite templating language. For the purpose of this documentation we will use [Pug](https://pugjs.org/) but you are free to pick whichever one you prefer, even native html.
+
+```pug
+d#0 Hello world
+  d.1 A good day to you too, sir!
+  d.2 Bye.
+d#1 Thanks!
+  d.2
+d#2 Bye
+```
+
+```js
+import helloWorld from 'hello-world.pug';
+import RPGDialogue from 'rpg-dialogue';
+
+let dialogue = new RPGDialogue(helloWorld());
+dialogue.interact(0);
+```
+
+### ID
+
+Use an ID to define a point in your dialogue that can be interacted with.
+
+```pug
+d#0 Hello world
+```
+
+### Class
+
+Use a class whenever an end-point should continue with a different point in your dialogue, these must point to an existing id
+
+```pug
+d#0 Hello world
+  d.1 Thanks
+
+d#1 Bye!
+```
+
+### Linear text
+
+ID's can be omitted for non-branching dialogue trees
+
+```pug
+d#0 Stay a while and listen
+  d I have an amazing story to tell you
+   d But time is running short
+    d.1 Ok...
+
+d#1 Bye!
+```
+
+### Conditions
+
+Conditions can be defined to control which branches can be accessed by the dialogue. This lets your application have control over when a specific dialogue option is available or not.
+
+```pug
+d#0 Are you alive?
+  d.1(if='IS_ALIVE') Yes!
+  d.2 Maybe...
+
+d#1 Cool!
+d#2 Hmmmm...
+```
+Then, in your code:
+
+```js
+const CONDITIONS = {
+  IS_ALIVE: () => player.hp > 0,
+  IS_PLAYER: true
+}
+
+let response = dialogue.interact(0, CONDITIONS);
+```
+
+The conditions must map to a _truthy_ value or a function that returns a _truthy_ value
+
+
+### Actions
+
+Actions can be defined to trigger when a certain branch is reached in the dialogue. This lets the dialogue trigger functions within your application.
+
+```pug
+d#0 Do you feel lucky? Punk.
+  d.1 Yes!
+  d.2 Nope...
+
+d#1 Okay then.
+
+d#2(then='KILL_PLAYER') Wrong answer!
+```
+
+```js
+const ACTIONS = {
+  KILL_PLAYER: () => player.kill()
+}
+
+let response = dialogue.interact(0, null, ACTIONS);
+```
+
+Any action that is not a valid function will be ignored
---
+++ src/dialogue.js
@@ -0,0 +1,140 @@
+export default class Dialogue {
+
+  //create a new Dialogue tree from html data
+  //see documentation for how to construct html
+  constructor (html) {
+    this.template = document.createElement('template');
+    this.template.innerHTML = html;
+    this.template = this.template.content;
+
+    this.currentOptions = [];
+  }
+
+  //interact with the dialogue
+  interact (id, CONDITIONS, ACTIONS) {
+    id =  Dialogue.parseID(id);
+
+    let element;
+
+    if (id === null) {
+      element = Array
+        .from(this.current.childNodes)
+        .filter(node => node.nodeType === node.ELEMENT_NODE)[0];
+    } else {
+      element = this.template.getElementById(id);
+    }
+
+    this.current = element;
+
+    const text = Dialogue.getText(this.current);
+
+    let responses = Dialogue.getOptions(this.current);
+    responses = this.validateOptions(responses);
+    responses = Dialogue.conditionOptions(responses, CONDITIONS);
+
+    this.triggerActions(id, ACTIONS);
+    this.currentOptions = responses;
+
+    if (!text && responses.length === 0) {
+      throw new Error('Dialogue Error - dead end');
+    }
+
+    return {
+      text: text,
+      responses: responses
+    };
+  }
+
+  //triggers actions for the current interaction
+  triggerActions (id, ACTIONS) {
+    if (!ACTIONS) {
+      return;
+    }
+
+    let actions = this.currentOptions
+      .filter(option => option.id === id)
+      .map(option => option.action);
+
+    actions.push(this.current.getAttribute('then'));
+
+    actions
+      .map(action => ACTIONS[action])                             //map to functions
+      .filter(action => typeof action === 'function')             //filter non-functions
+      .filter((action, i, list) => list.indexOf(action) === i)    //filter duplicates
+      .forEach(action => action());                               //trigger actions
+  }
+
+  //removes invalid options from a list of options
+  validateOptions (list) {
+    return list.filter(option => {
+      if (option.id === null && Dialogue.hasChildOptions(this.current)) {
+        return true;
+      } else if (this.template.getElementById(option.id)) {
+        return true;
+      } else {
+        console.warn('Invalid dialogue option', option);
+      }
+    })
+  }
+
+  //removes options from a list of options that don't meet their condition
+  static conditionOptions (list, CONDITIONS) {
+    if (!CONDITIONS) {
+      return list;
+    }
+
+    return list.filter(option => {
+      if (!option.condition) {
+        return true;
+      } else if (typeof CONDITIONS[option.condition] === 'function') {
+        return CONDITIONS[option.condition]();
+      } else {
+        return CONDITIONS[option.condition];
+      }
+    })
+  }
+
+  //parse an ID, making sure it's a number or null
+  static parseID (id) {
+    id = parseInt(id, 10);
+
+    if (Number.isNaN(id)) {
+      id = null;
+    }
+
+    return id;
+  }
+
+  //checks whether a node has options
+  static hasChildOptions (node) {
+    return Array.from(node.childNodes).filter(node => node.nodeType === node.ELEMENT_NODE).length > 0;
+  }
+
+  //returns the available options from a node
+  static getOptions (node) {
+    const options = Array.from(node.childNodes).map(node => {
+      if (node.nodeType === node.ELEMENT_NODE) {
+        return {
+          id: Dialogue.parseID(node.id || node.className),
+          text: Dialogue.getText(node),
+          condition: node.getAttribute('if') || null,
+          action: node.getAttribute('then') || null
+        }
+      }
+    })
+    .filter(option => Boolean(option));
+
+    if (options.length === 1) {
+      options[0].text = null;
+    }
+
+    return options;
+  }
+
+  //returns the text of a node, ignoring its children
+  static getText (node) {
+    return Array.from(node.childNodes).reduce((text, val) => {
+      return text + (val.nodeType === val.TEXT_NODE ? val.textContent : '');
+    }, '');
+  }
+}
